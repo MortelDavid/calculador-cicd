0422a678abbe3c035017ceb65207a2d52a220e8a
MortelDavid <morteldavi@gmail.com>
2025-04-23 14:29:49 -0300

    Create soma.c

---
+++ soma.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int somar(int a, int b){
+  return a + b;
+}
+
+int main(){
+  int num1 = 10;
+  int num2 = 5;
+  int resultado = somar(num1, num2);
+  printf("A soma de %d e %d é: %d\n", num1, num2, resultado);
+  return 0;
+}
